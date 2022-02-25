0a02dc7580c3d3aa6c5b1d97466f0c4d0665f12e
joacotol <joacochrist@gmail.com>
2022-02-25 14:13:24 -0500

    Found solution

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,7 +1,27 @@
 class ReverseString {
 
-    String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public static void main(String[] args) {
+        // Example
+        String message = "robot";
+        System.out.println(reverse(message));
     }
-  
+
+    public static String reverse(String inputString) {
+        // convert string into an array of characters
+        char[] singleLetters = new char[inputString.length()];
+        for (int i = 0; i < inputString.length(); i++) {
+            singleLetters[i] = inputString.charAt(i);
+        }
+
+        // Swaps the current character to the last index of the character that decrements for each iteration
+        char swap;
+        for (int j = 0; j < singleLetters.length / 2; j++) {
+            swap = singleLetters[j];
+            singleLetters[j] = singleLetters[(singleLetters.length - 1) - j];
+            singleLetters[(singleLetters.length - 1) - j] = swap;
+        }
+        // Converts the character array to return a String
+        return String.valueOf(singleLetters);
+    }
+
 }
